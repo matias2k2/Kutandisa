741835f84c8fb5ccaec8fd7939c93a10b82281a3
Matias G Adelina <98082081+matias2k2@users.noreply.github.com>
2026-05-12 09:05:29 +0100

    criacao do projecto spring boot a nossa api

---
+++ Kutandisa/src/main/java/tinario9954/gmail/com/Kutandisa/KutandisaApplication.java
@@ -0,0 +1,13 @@
+package tinario9954.gmail.com.Kutandisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KutandisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KutandisaApplication.class, args);
+	}
+
+}
---
+++ Kutandisa/src/test/java/tinario9954/gmail/com/Kutandisa/KutandisaApplicationTests.java
@@ -0,0 +1,13 @@
+package tinario9954.gmail.com.Kutandisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KutandisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
